85d5429dfc58bc03748e3373583220eb5c69e9eb
hjpotter92 <hjpotter92@gmail.com>
2015-02-11 09:57:39 +0530

    Implementing client interface

--- client.py
+++ client.py
@@ -1,8 +1,52 @@
 import socket
+from pygame.locals import *
+from pygame import font, event as pyevent, display, draw
 from player import player
 
+def GetKey():
+	while True:
+		event = pyevent.poll()
+		if event.type == KEYDOWN:
+			if event.key in range( 256, 266 ):
+				# For keys pressed in numberpad
+				return event.key - 208
+			return event.key
+
+def BoxDisplay( scr, msg ):
+	text, w, h = font.Font( None, 24 ), scr.get_width(), scr.get_height()
+	pos_x, pos_y = w / 2 - 200, h / 2 - 30
+	draw.rect( scr, (128, 128, 128), (pos_x, pos_y, 400, 60), 0 )
+	draw.rect( scr, (255, 255, 255), (pos_x - 2, pos_y - 2, 404, 64), 1 )
+	if len( msg ) > 0:
+		scr.blit( text.render(msg, 1, (255, 255, 255)), (pos_x, pos_y) )
+	display.flip()
+
+def AskQuestion( screen, question ):
+	font.init()
+	response = []
+	while True:
+		BoxDisplay( screen, question + ": " + ''.join(response) )
+		inp = GetKey()
+		if inp == K_BACKSPACE:
+			response = response[:-1]
+		elif inp == K_ESCAPE:
+			return ''
+		elif inp == K_RETURN or inp == K_KP_ENTER:
+			break
+		elif inp == K_KP_PERIOD:
+			response.append( '.' )
+		elif inp <= 127:
+			response.append( chr(inp) )
+	return ''.join( response )
+
 class client:
-	def __init__( self ):
-		name = raw_input( "Enter a nickname: " )
+	def __init__( self, name, srvIP ):
 		ip = socket.gethostbyname( socket.gethostname() )
 		self.__gamer = player( name, ip )
+		self.__server_ip = srvIP
+
+if __name__ == "__main__":
+	window = display.set_mode( (600, 400) )
+	alias = AskQuestion( window, "Your nickname" )
+	server_ip = AskQuestion( window, "Server IP" )
+	c = client( alias.strip(), server_ip.strip() )
--- player.py
+++ player.py
@@ -1,4 +1,10 @@
 class player:
 	def __init__( self, alias, IP = '' ):
 		self.__alias = alias
-		self.__ip = ip
+		self.__ip = IP
+
+	def GetIP( self ):
+		return self.__ip
+
+	def GetNick( self ):
+		return self.__alias
