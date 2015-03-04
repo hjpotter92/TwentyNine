07571482cd3a8f686e156e270f975a1e9a853be7
hjpotter92 <hjpotter92@gmail.com>
2015-03-05 03:34:08 +0530

    Rewrite connect.py based on Stack Overflow question
    Ref.: https://stackoverflow.com/q/28583242/1190388

--- connect.py
+++ connect.py
@@ -1,18 +1,25 @@
 from json import dumps, loads
+from struct import calcsize, pack, unpack
 
 def send( channel, message ):
 	try:
-		channel.send( dumps(message) )
+		msg = dumps( message )
+		channel.send( pack("i", len(msg)) + msg )
 		return True
 	except OSError as e:
 		print e
 		return False
 
-def receive( channel, packet_size = 1024 ):
+def receive( channel ):
 	try:
-		data = channel.recv( int(packet_size) )
-		if not data:
-			return None
+		size = unpack( "i", channel.recv(calcsize("i")) )[0]
+		data = ""
+		while len(data) < size:
+			msg = channel.recv( size - len(data) )
+			if not msg:
+				return None
+			data += msg
+		print data
 		return loads( data.strip() )
 	except OSError as e:
 		print e
